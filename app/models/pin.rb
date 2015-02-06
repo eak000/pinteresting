aa565bec05a0241e5a73b27e5cda45663a576755
Emily Kelton <emilyk000@gmail.com>
2015-02-06 13:08:52 -0800

    add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
