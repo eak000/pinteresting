bead4b687ed073373e8ce591ec1da966bf281c18
Emily Kelton <emilyk000@gmail.com>
2015-02-06 00:25:38 -0800

    disabled delete

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  # before_action :correct_user, only: [:edit, :update, :destroy]
-  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -45,10 +45,10 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    # def correct_user
-    #   @pin = current_user.pins.find_by(id: params[:id])
-    #   redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-    # end
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
